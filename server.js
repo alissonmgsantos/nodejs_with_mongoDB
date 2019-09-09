a680274dfbeb7508a4dcf384a605087b0d074fac
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-09 10:28:00 -0300

    Configuração do mongoose e criação do model de produto

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+require('./src/models/Product');
+
+const Product = mongoose.model('Product');
+
+// Primeira rota
+app.get('/', (req, res) => {
+    Product.create({
+        title: 'Mongo',
+        description: 'MongoDB'
+    });
+    return res.send('OK');
+});
 
 app.listen(3000);
---
+++ src/models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+const ProductSchema = new mongoose.Schema({
+    title: { type: String, require: true },
+    description: { type: String, require: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+mongoose.model('Product', ProductSchema);
